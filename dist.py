28e46426ab3363220f70e86cbf5cc2b8cd3c1574
rapidswap <73596570+rapidswap@users.noreply.github.com>
2024-03-20 19:21:31 +0900

    Update dist.py

--- dist.py
+++ dist.py
@@ -1,12 +1,26 @@
 import numpy as np
 
-A=np.array([[1,2,3],[4,5,6],[7,8,9]])
-B=np.array([[10,11,12],[13,14,15],[16,17,18]])
+A=np.array([[0,1,1,0],
+            [0,1,1,0],
+            [0,1,1,0],
+            [0,1,1,0]])
+B=np.array([[0,0,0,0],
+            [1,1,1,1],
+            [1,1,1,1],
+            [0,0,0,0]])
+C=np.array([[0,1,1,0],
+            [0,1,1,0],
+            [0,0,1,1],
+            [0,1,1,0]])
 def dist(A,B,row,cols):
     tmp=0
     for i in range(row):
         for j in range(cols):
             tmp += np.power((A[i][j]-B[i][j]),2)
     return tmp
-C=dist(A,B,3,3)
-print(C)
+D=dist(A,B,4,4)
+E=dist(B,C,4,4)
+F=dist(A,C,4,4)
+print(D)
+print(E)
+print(F)
