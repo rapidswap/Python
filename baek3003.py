029194a6943b2d5e8a78519118b37732b75dd7b4
rapidswap <curudow7988@gmail.com>
2024-03-27 19:52:10 +0900

    modified:   baek3003.py

--- baek3003.py
+++ baek3003.py
@@ -3,10 +3,10 @@ white_chess=[]
 white=[1,1,2,2,2,8]
 need_white=[0,0,0,0,0,0]
 
-pieces_input = input("흰색 피스 킹, 퀸, 룩, 비숍, 나이트, 폰: ")
+pieces_input = input()
 white_chess = list(map(int, pieces_input.split()))
 
 for j in range(len(range(len(white)))):
     if white[j] !=need_white[j]:
         need_white[j]=int(white[j])-int(white_chess[j])
-print(need_white)
+print(need_white[0],need_white[1],need_white[2],need_white[3],need_white[4],need_white[5])
