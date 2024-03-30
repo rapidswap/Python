f875aa4675e16fedb92fdf56bce1f1da2db29203
rapidswap <curudow7988@gmail.com>
2024-03-30 23:53:26 +0900

    new file:   AI_week4_2.py

---
+++ AI_week4_2.py
@@ -0,0 +1,7 @@
+#K-mean 알고리즘
+
+for epoch in range(10000):
+    for i in range(100):
+        #classnum = min(p[i],m[0],m[1],m[2])
+        #new_mclass+=p[i]
+    #m[1]=new_m[1]/ member_num ... loop
