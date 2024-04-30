c5b253f8b8ddee2b71430899ab6cdba159c90e29
rapidswap <curudow7988@gmail.com>
2024-04-30 15:28:43 +0900

    ``	modified:   BaekJoon_problem/baek1812.py

--- BaekJoon_problem/baek1812.py
+++ BaekJoon_problem/baek1812.py
@@ -1,4 +1,6 @@
 N = int(input())
+if N % 2 == 0 :
+    exit()
 Candy=[]
 Com_Candy=[]
 for i in range(N):
@@ -12,15 +14,10 @@ if candy1 > candy2:
 elif candy2 > candy1:
     X=candy2-candy1
 elif candy1 == candy2:
-    candy1=Candy[0]
-    candy2=Candy[2]
-    if candy1 > candy2:
-        X=candy1-candy2
-    elif candy2 > candy1:
-        X=candy2-candy1
-        
-Com_Candy.append(int(X)
-                 )
+    X=0
+
+Com_Candy.append(int(X))
+
 for i in range(N-1):
     Com_Candy.append(int(Candy[i]-Com_Candy[i]))
 
